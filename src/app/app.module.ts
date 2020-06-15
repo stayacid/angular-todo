d3009d161cf148f261fb371f8faa0ec8040b971c
stayacid <stayacid@gmail.com>
2020-06-15 10:14:50 +0300

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,20 @@ import {
 import { HttpClientModule } from '@angular/common/http';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 
+//auth
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGX8jfeiFVmqzS-AmXXtjSoOBNQ29Knlk",
+  authDomain: "angular-todo-c6252.firebaseapp.com",
+  databaseURL: "https://angular-todo-c6252.firebaseio.com",
+  projectId: "angular-todo-c6252",
+  storageBucket: "angular-todo-c6252.appspot.com",
+  messagingSenderId: "169983727754",
+  appId: "1:169983727754:web:fb62b87a91cfaa741f3573"
+};
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,6 +43,8 @@ import { TodoFormComponent } from './components/todo-form/todo-form.component';
     TodosComponent,
   ],
   imports: [
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
     BrowserAnimationsModule,
     BrowserModule,
     HttpClientModule,
